246323cd9b4ae373a332b717110aa216fe713f61
Mustafa-Kheda <mustafa.kheda@cloudflex.co.in>
2025-12-08 02:28:41 +0530

    refactor(data): replace FakeStoreAPI fetch with local static JSON file
    - Added products.json under app/data for stable mock product data
    - Removed external API fetch to prevent SSR failures and Cloudflare blocks
    - Updated product page to import static JSON directly instead of fetching
    - Improved reliability, performance, and deployment stability on Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Breadcrumb from "@/components/Breadcrumb";
 import ProductList from "@/components/ProductList";
+import products from "../../public/data/products.json";
 interface Product {
   id: number;
   title: string;
@@ -8,35 +9,14 @@ interface Product {
   category: string;
 }
 
-async function fetchProducts(): Promise<Product[]> {
-  const res = await fetch("https://fakestoreapi.com/products", {
-    cache: "no-store",
-    next: { revalidate: 0 },
-  });
-
-  // Handle API failure (HTML returned instead of JSON)
-  if (!res.ok) {
-    const errorText = await res.text(); // This will show the HTML returned
-    console.error("FakeStore API ERROR:", errorText);
-    return []; // return empty array instead of crashing SSR
-  }
-
-  try {
-    return await res.json();
-  } catch (e) {
-    console.error("JSON PARSE ERROR:", e);
-    return [];
-  }
-}
-
 export default async function Home() {
-  const products = await fetchProducts();
+
 
   return (
     <main className="page">
       {/* HERO SECTION */}
       <section className="hero">
-        <Breadcrumb label="SHOP"/>
+        <Breadcrumb label="SHOP" />
         <h1>Discover Our Products</h1>
         <p className="hero-body">
           Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus scelerisque. Dolor integer scelerisque nibh amet mi ut elementum dolor.
